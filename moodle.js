3aede2a07f191f66096257e2c9067216200ab177
Nikhil Raj <darklacerta7817@gmail.com>
2021-04-21 16:05:26 +0530

    moodle works with hardcoded password

--- moodle.js
+++ moodle.js
@@ -1,7 +1,20 @@
-captcha = document.getElementById("login").textContent;
-const regexpp = /(\d\d\d|\d\d|\d)/g ;
-let numbers = [...captcha.matchAll(regexpp)];
+captcha = document.getElementById("login").textContent;//get the captcha text
+numbers = [...captcha.matchAll(/(\d\d\d|\d\d|\d)/g)];//get both numbers in captcha using regex
 
-console.log(numbers[0]);
-console.log(numbers[1]);
-console.log(Number(numbers[0][0])+Number(numbers[1][0]));
+//get type of captcha and solve
+if(/add/.test(captcha))
+    captcha = Number(numbers[0][0])+Number(numbers[1][0]);
+if(/subtract/.test(captcha))
+    captcha = Number(numbers[0][0])-Number(numbers[1][0]);
+if(/first/.test(captcha))
+    captcha = Number(numbers[0][0]);
+if(/second/.test(captcha))
+    captcha = Number(numbers[1][0]);            
+
+    document.getElementById("valuepkg3").value = String(captcha);//entering the captcha
+
+    document.getElementById("username").value="username";
+    document.getElementById("password").value="password";
+
+    
+    document.getElementById("loginbtn").click();//click on login button
