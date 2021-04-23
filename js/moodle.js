849258f3d5911a1bd15893165bea162c4d234279
Nikhil Raj <darklacerta7817@gmail.com>
2021-04-23 13:53:08 +0530

    fixed storage bugs

--- js/moodle.js
+++ js/moodle.js
@@ -11,12 +11,15 @@ if(/first/.test(captcha))
 if(/second/.test(captcha))
     captcha = Number(numbers[1][0]);            
 
+   
+//retrieve username and password from local storage and inputing
+  
+chrome.storage.local.get(['moodleuname','moodlepass'],function(result){
+
+    document.getElementById("username").value=result.moodleuname;
+    document.getElementById("password").value=result.moodlepass;
     document.getElementById("valuepkg3").value = String(captcha);//entering the captcha
-
-
-    //retrieve username and password from storage and inputing
-    chrome.storage.local.get(['moodleuname'],function(result){document.getElementById("username").value=result.moodleuname;});
-    chrome.storage.local.get(['moodlepass'],function(result){document.getElementById("password").value=result.moodlepass;});
-    
-    //pressing login button
     document.getElementById("loginbtn").click();//click on login button
+}
+);
+    
--- js/options.js
+++ js/options.js
@@ -1,5 +1,5 @@
 document.getElementById("moodlesave").onclick=moodlesave;
 function moodlesave(){
-  chrome.storage.local.set({moodleuname:document.getElementById("moodleuname").value});
+  window.localStorage.setItem("moodleuname",document.getElementById("moodleuname").value);
   chrome.storage.local.set({moodlepass:document.getElementById("moodlepass").value});
 }
