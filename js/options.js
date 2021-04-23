7b62603ed50c320174b220fcc0df1a1284f8fb36
Nikhil Raj <darklacerta7817@gmail.com>
2021-04-23 22:00:07 +0530

    Code cleaning

--- js/options.js
+++ js/options.js
@@ -1,5 +1,20 @@
+AES_Init();
+
+
+
+
 document.getElementById("moodlesave").onclick=moodlesave;
 function moodlesave(){
   chrome.storage.local.set({moodleuname:document.getElementById("moodleuname").value});
   chrome.storage.local.set({moodlepass:document.getElementById("moodlepass").value});
+}
+document.getElementById("webmailsave").onclick=webmailesave;
+function webmailsave(){
+  chrome.storage.local.set({webmailuname:document.getElementById("webmailuname").value});
+  chrome.storage.local.set({webmailpass:document.getElementById("webmailpass").value});
+}
+
+document.getElementById("master").onclick=mastersave;
+function mastersave(){
+  chrome
 }
