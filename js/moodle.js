8b683eb94c3a5ec50fd43f09d39f1a8035c45434
Nikhil Raj <darklacerta7817@gmail.com>
2021-09-30 17:23:55 +0530

    Css grid

--- js/moodle.js
+++ js/moodle.js
@@ -1,20 +1,24 @@
-window.onload = function(){
-captcha = document.getElementById("login").textContent;//get the captcha text
-numbers = [...captcha.matchAll(/(\d\d\d|\d\d|\d)/g)];//get both numbers in captcha using regex
+window.onload = function () {
+  captcha = document.getElementById("login").textContent; //get the captcha text
+  numbers = [...captcha.matchAll(/(\d\d\d|\d\d|\d)/g)]; //get both numbers in captcha using regex
 
-//get type of captcha and solve
-if(/add/.test(captcha))
-    captcha = Number(numbers[0][0])+Number(numbers[1][0]);
-if(/subtract/.test(captcha))
-    captcha = Number(numbers[0][0])-Number(numbers[1][0]);
-if(/first/.test(captcha))
-    captcha = Number(numbers[0][0]);
-if(/second/.test(captcha))
-    captcha = Number(numbers[1][0]);            
+  //get type of captcha and solve
+  if (/add/.test(captcha))
+    captcha = Number(numbers[0][0]) + Number(numbers[1][0]);
+  if (/subtract/.test(captcha))
+    captcha = Number(numbers[0][0]) - Number(numbers[1][0]);
+  if (/first/.test(captcha)) captcha = Number(numbers[0][0]);
+  if (/second/.test(captcha)) captcha = Number(numbers[1][0]);
 
-   
-//retrieve username and password from local storage and inputing
+  //retrieve username and password from local storage and inputing
 
-
-fill("username","moodleuname","password","moodlepass",captcha,"valuepkg3","loginbtn");
-   }
+  fill(
+    "username",
+    "moodleuname",
+    "password",
+    "moodlepass",
+    captcha,
+    "valuepkg3",
+    "loginbtn"
+  );
+};
--- js/options.js
+++ js/options.js
@@ -1,25 +1,51 @@
-document.getElementById("mastersave").onclick=mastersave;
-function mastersave(){
-  changepass("master",document.getElementById("oldmaster").value,document.getElementById("master").value);
-}
+window.onload = function () {
+  document.getElementById("mastersave").onclick = mastersave;
+  function mastersave() {
+    changepass(
+      "master",
+      document.getElementById("oldmaster").value,
+      document.getElementById("master").value
+    );
+  }
 
-document.getElementById("moodlesave").onclick=moodlesave;
-function moodlesave(){
-  change("moodleuname",document.getElementById("moodlemasterpassword").value,document.getElementById("moodleuname").value);
-  change("moodlepass",document.getElementById("moodlemasterpassword").value,document.getElementById("moodlepass").value);
-}
-document.getElementById("webmailsave").onclick=webmailsave;
-function webmailsave(){
-  change("webmailuname",document.getElementById("webmailmasterpassword").value,document.getElementById("webmailuname").value);
-  change("webmailpass",document.getElementById("webmailmasterpassword").value,document.getElementById("webmailpass").value);
-}
+  document.getElementById("moodlesave").onclick = moodlesave;
+  function moodlesave() {
+    change(
+      "moodleuname",
+      document.getElementById("moodlemasterpassword").value,
+      document.getElementById("moodleuname").value
+    );
+    change(
+      "moodlepass",
+      document.getElementById("moodlemasterpassword").value,
+      document.getElementById("moodlepass").value
+    );
+  }
+  document.getElementById("webmailsave").onclick = webmailsave;
+  function webmailsave() {
+    change(
+      "webmailuname",
+      document.getElementById("webmailmasterpassword").value,
+      document.getElementById("webmailuname").value
+    );
+    change(
+      "webmailpass",
+      document.getElementById("webmailmasterpassword").value,
+      document.getElementById("webmailpass").value
+    );
+  }
 
-document.getElementById("kerberossave").onclick=kerberossave;
-function kerberossave(){
-  change("kerberosuname",document.getElementById("kerberosmasterpassword").value,document.getElementById("kerberosuname").value);
-  change("kerberospass",document.getElementById("kerberosmasterpassword").value,document.getElementById("kerberospass").value);
-}
-
-
-     
-
+  document.getElementById("kerberossave").onclick = kerberossave;
+  function kerberossave() {
+    change(
+      "kerberosuname",
+      document.getElementById("kerberosmasterpassword").value,
+      document.getElementById("kerberosuname").value
+    );
+    change(
+      "kerberospass",
+      document.getElementById("kerberosmasterpassword").value,
+      document.getElementById("kerberospass").value
+    );
+  }
+};
